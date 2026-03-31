06a8cb678c46a8c9e27a934edcfa101b61244d95
130reevtrb <b2029reftapia@pshs.edu.ph>
2026-03-31 23:30:43 +0800

    commit

--- public/3rdGraded/script.js
+++ public/3rdGraded/script.js
@@ -1,16 +1,19 @@
 function plotPoint(x0, y0, x, y) {
     console.log(x0, y0, x, y); // checks the arguments passed to this function
 
-    var in1 = document.getElementById("in1");
-    var out1 = document.getElementById("out1");
+    const in1 = document.getElementById("in1");
+    const out1 = document.getElementById("out1");
+    const plane = document.getElementById("coordinatePlane");
 
     in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
-    var point = document.createElement('div');  
+
+    const point = document.createElement('div');  
     point.className = 'point';
     /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
-    document.getElementById('coordinatePlane').appendChild(point);
+    point.style.position = "absolute";
+    point.style.left = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
+    point.style.bottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+    plane.appendChild(point);
 
     if (x > x0 && y > y0){
       out1.innerHTML = "NE";
